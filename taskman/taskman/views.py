01acb82c111d8ffe58479b6c80f4af9b0a37fbe9
Kabir <kabirsharma2905@gmail.com>
2024-03-22 16:24:55 +0530

    Fixed static files

--- taskman/taskman/views.py
+++ taskman/taskman/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import redirect, render, HttpResponse, Http404
-from django.http import FileResponse
-import os
-from django.conf import settings
+from django.shortcuts import redirect
 
 def landing_view(request):
     if not request.user or not request.user.is_authenticated:
@@ -9,11 +6,3 @@ def landing_view(request):
     else:
         return redirect('task-home')
     
-
-# def serve_media(request, path):
-#     file_path = os.path.join(settings.MEDIA_ROOT, path)
-#     if os.path.exists(file_path):
-#         with open(file_path, 'rb') as file:
-#             return HttpResponse(file.read(), content_type="image/jpeg")
-#     else:
-#         raise Http404("Image does not exist")
