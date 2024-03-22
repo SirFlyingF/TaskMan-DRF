65fb6a0ee90ac8f12953a9cbcfd8792f7c4d6a84
Kabir <kabirsharma2905@gmail.com>
2024-03-22 11:35:03 +0530

    Saving...

--- taskman/taskman/settings.py
+++ taskman/taskman/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -138,8 +139,8 @@ LOGIN_REDIRECT_URL = 'profile'
 # check on login required view
 LOGIN_URL = 'login'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-MEDIA_URL = 'media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/' 
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
